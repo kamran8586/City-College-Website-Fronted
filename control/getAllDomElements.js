8737a3153405c6ccbec384f67332a739cb71c820
Kamran <muhammadkamran8586@gmail.com>
2022-02-06 13:00:42 +0500

    Add new javascript file for initilize all dom elements.

---
+++ control/getAllDomElements.js
@@ -0,0 +1,21 @@
+/* Variable initialization */
+
+let announcement = document.querySelector('.announcement');
+let openButton = document.querySelector('.openButton');
+let navbarItemResponsive = document.querySelector('.navbar-items-responsive');
+let navItems = document.querySelectorAll('.nav-item');
+let home = document.querySelector('.home');
+let allContent = document.querySelector('.allContent');
+let contentPageArray = [homePage, announcementPage, aboutPage, servicePage];
+const closeModal = document.querySelector('.closeModal');
+let createAnnouncementModalBox = document.querySelector('.createAnnouncementModalBox');
+const openAnnouncementModalBox = document.querySelector('.openAnnouncementModalBox');
+const createPostButton = document.querySelector('.createPostButton');
+
+let blogLocalStorageData = localStorage;
+const blogTitle = document.querySelector('.title');
+const blogContent = document.querySelector('.content');
+const inputFields = document.querySelectorAll('.inputField');
+
+
+console.log("hi");
